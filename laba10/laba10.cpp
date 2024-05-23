926434bf74215f2a9dc79942814eddd6ec0e45b7
Danylo Masiyk <163041483+DanyloIKM723a@users.noreply.github.com>
2024-05-23 20:50:18 +0300

    Update laba10.cpp

--- laba10/laba10.cpp
+++ laba10/laba10.cpp
@@ -1,53 +1,47 @@
-﻿#include <iostream>
-#include <vector>
+#include <iostream>
 #include <string>
 
 using namespace std;
-
 
 struct Faculty {
     string name;
     int backlog;
 
-
     Faculty(string n, int b) : name(n), backlog(b) {}
 };
 
 int main() {
-
-    vector<Faculty> faculties;
-
-
-    faculties.push_back(Faculty("Faculty 1", 10));
-    faculties.push_back(Faculty("Faculty 2", 5));
-    faculties.push_back(Faculty("Faculty 3", 8));
-    faculties.push_back(Faculty("Faculty 4", 3));
-    faculties.push_back(Faculty("Faculty 5", 12));
-
+    // Оскільки не можна використовувати vector, використовуємо масив
+    const int numFaculties = 5;
+    Faculty faculties[numFaculties] = {
+        Faculty("Faculty 1", 10),
+        Faculty("Faculty 2", 5),
+        Faculty("Faculty 3", 8),
+        Faculty("Faculty 4", 3),
+        Faculty("Faculty 5", 12)
+    };
 
     cout << "Full information about faculties:" << endl;
-    for (const auto& faculty : faculties) {
-        cout << "Faculty: " << faculty.name << ", Backlog: " << faculty.backlog << endl;
+    for (int i = 0; i < numFaculties; ++i) {
+        cout << "Faculty: " << faculties[i].name << ", Backlog: " << faculties[i].backlog << endl;
     }
-
 
     int maxBacklog = faculties[0].backlog;
     string maxBacklogFaculty = faculties[0].name;
-    for (const auto& faculty : faculties) {
-        if (faculty.backlog > maxBacklog) {
-            maxBacklog = faculty.backlog;
-            maxBacklogFaculty = faculty.name;
+    for (int i = 1; i < numFaculties; ++i) {
+        if (faculties[i].backlog > maxBacklog) {
+            maxBacklog = faculties[i].backlog;
+            maxBacklogFaculty = faculties[i].name;
         }
     }
     cout << "Faculty with the maximum backlog: " << maxBacklogFaculty << " (" << maxBacklog << ")" << endl;
 
-
     int minBacklog = faculties[0].backlog;
     string minBacklogFaculty = faculties[0].name;
-    for (const auto& faculty : faculties) {
-        if (faculty.backlog < minBacklog) {
-            minBacklog = faculty.backlog;
-            minBacklogFaculty = faculty.name;
+    for (int i = 1; i < numFaculties; ++i) {
+        if (faculties[i].backlog < minBacklog) {
+            minBacklog = faculties[i].backlog;
+            minBacklogFaculty = faculties[i].name;
         }
     }
     cout << "Faculty with the minimum backlog: " << minBacklogFaculty << " (" << minBacklog << ")" << endl;
